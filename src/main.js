e0a4441c9e287f725bc2782d033ff3d1221d10f5
Thais Alves Pereira <tathyflor@gmail.com>
2023-05-24 20:46:03 -0300

    teste do tipo

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> c72ad4d701d422f172e812ead39dd996d2194e62
 import {filtroSignBaixo, filtroSignCima, sortByType, SelecaoNome} from './data.js';
 
 import data from './data/tarot/tarot.js';
